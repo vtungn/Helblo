0b1f54041f3fabaf8dfcfd463c1f7b178af17264
vtungn <vtungn@github.com>
2014-02-23 03:12:19 +0700

    bullet modified

--- Assets/Script/BulletScript.cs
+++ Assets/Script/BulletScript.cs
@@ -8,14 +8,22 @@ public class BulletScript : MonoBehaviour {
 	/// Projectile damage player or enemies?
 	/// </summary>
 	public bool isEnemyShot = false;
-	public Vector2 direction = new Vector2(10,0);
+	public Vector2 direction;
+	public Vector2 speed = new Vector2(1.5f,1.5f);
+
 	// Use this for initialization
 	void Start () {
-		Destroy(gameObject, 1); // 20sec avoid leaking memory
+		Destroy(gameObject, 2); // 20sec avoid leaking memory
+		// point the bullet toward enemy
+		float RotateAngle = Vector2.Angle (Vector2.up, direction);
+		if (direction.x > 0)
+			RotateAngle = -RotateAngle;
+		transform.eulerAngles =new Vector3 (0, 0, RotateAngle);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rigidbody2D.velocity = direction;
+		rigidbody2D.velocity = new Vector2(direction.x * speed.x , direction.y * speed.y );
 	}
 }
--- Assets/Script/WeaponController.cs
+++ Assets/Script/WeaponController.cs
@@ -10,29 +10,41 @@ public class WeaponController : MonoBehaviour {
 	/// <summary>
 	/// Cooldown in seconds between two shots
 	/// </summary>
-	public float shootingRate = 0.25f;
-
+	public float shootingSpeed = 0.005f;
+	private float shootCooldown;
 	void Start () {
-
+		shootCooldown = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (shootCooldown > 0)
+		{
+			shootCooldown -= Time.deltaTime;
+		}
 	}
 
 	public void shoot () {
-		var shotTransform = Instantiate(shotPrefab) as Transform;
-		
-		// Assign position
-		shotTransform.position = transform.position;
-		
-		// The is enemy property
-		BulletScript shot = shotTransform.gameObject.GetComponent<BulletScript>();
-		if (shot != null)
-		{
-			shot.direction = transform.up;
+		if (CanAttackNow()) {
+			shootCooldown = shootingSpeed;
+			var shotTransform = Instantiate (shotPrefab) as Transform;
+
+			// Assign position
+			shotTransform.position = transform.position;
+
+			// The is enemy property
+			BulletScript shot = shotTransform.gameObject.GetComponent<BulletScript> ();
+			if (shot != null) {
+					//shoot arr right out the head of Poi
+					shot.direction = transform.up;
+			}
 		}
 
+	}
+
+	bool CanAttackNow () {
+		if (shootCooldown <= 0)
+						return true;
+		else return false;
 	}
 }
